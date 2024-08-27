1530250fa584e7a914acc6a4e024a0dd82c93160
Jiwuu <142757244+Jiwuuuu@users.noreply.github.com>
2024-08-27 15:53:47 +0800

    add all the flutter framework

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:cinesphere/screens/home_screen.dart';
+void main() {
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Poppins'),
+      home: const HomeScreen(),
+    );
+  }
+}
