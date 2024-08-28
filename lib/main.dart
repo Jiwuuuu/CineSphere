c67f1fd737043255949d26bbf5637cbd283d6f22
unknown <juna.cabrera.up@phinmaed.com>
2024-08-29 02:27:07 +0800

    welcome and landing page

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
+import 'package:cinesphere/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:cinesphere/screens/home_screen.dart';
+
+
 void main() {
-  runApp(MyApp());
+  runApp(MyApp(),);
 }
 
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: 'Poppins'),
-      home: const HomeScreen(),
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xFF212429),
+      ),
+      home: WelcomeScreen(),
     );
   }
 }
