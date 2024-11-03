86027aa42670cc8b40621e8c71eef44f3fcf0d47
Andrei. <zrojeat@gmail.com>
2024-11-03 20:37:24 +0800

    Update supabase_service.dart

--- lib/database/supabase_service.dart
+++ lib/database/supabase_service.dart
@@ -13,8 +13,8 @@ class SupabaseService {
 
   Future<void> initializeSupabase() async {
     await Supabase.initialize(
-      url: 'https://ugvcmfripsepimuwetav.supabase.co', // Supabase URL
-      anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVndmNtZnJpcHNlcGltdXdldGF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYyODM4MTAsImV4cCI6MjA0MTg1OTgxMH0.jdOtxDcv6ZkDlNCQrzEhxaoyWaji4ThzBE65AG0bpqo', // Supabase anon key
+      url: '', // Supabase URL
+      anonKey: '', // Supabase anon key
     );
   }
 
